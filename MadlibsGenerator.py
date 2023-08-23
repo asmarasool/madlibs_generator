cc53e811047ea7f102885172735f1c34b15f45fe
Nila Rasool <39612573+asmarasool@users.noreply.github.com>
2023-08-22 20:42:50 -0600

    Add files via upload

---
+++ MadlibsGenerator.py
@@ -0,0 +1,34 @@
+with open("story.txt", "r") as f:
+    story = f.read()
+
+# print(story)
+
+# this block of code will find the words in the story that need
+# be replaced with a value
+words = set()
+start_of_word = -1
+
+word_start = "["
+word_end = "]"
+
+for i, char in enumerate(story):
+    if char == word_start:
+        start_of_word = i
+
+    if char == word_end and start_of_word != -1:
+        word = story[start_of_word: i + 1]
+        words.add(word)
+        start_of_word = -1
+
+# answers = {"<name>": "Tim"} name is key, value is Tim
+# asnwers["<name>"]= "Nila"
+answers = {}
+for word in words:
+    user_entry = input("Enter a word for " + word + ": ")
+    answers[word] = user_entry
+
+for word in words:
+    story = story.replace(word, answers[word])
+
+print(story)
+
